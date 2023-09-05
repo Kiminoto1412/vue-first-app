e359f5a36a33dbc63ba869b1d1d2d1c2c4eec3f9
Paruj Labpattanapaisan <mammothz@Parujs-MacBook-Pro.local>
2023-09-06 01:47:19 +0700

    feat:using emit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,46 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Fav</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }}
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ phone }}</li>
+      <li><strong>Email:</strong>{{ email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "FriendContact",
+  // props: ["name", "phone", "email", "isFavorite"],
+  props: {
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    email: { type: String, required: true },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
