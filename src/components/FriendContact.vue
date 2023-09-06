905fdf32d61af0cf458158ef0db58cae063c8375
Paruj Labpattanapaisan <mammothz@Parujs-MacBook-Pro.local>
2023-09-07 01:43:14 +0700

    feat:using emit

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -27,6 +27,16 @@ export default {
       default: false,
     },
   },
+  emits: {
+    "toggle-favorite": function (id) {
+      if (id) {
+        return true;
+      } else {
+        console.warn("Id is missing");
+        return false;
+      }
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
